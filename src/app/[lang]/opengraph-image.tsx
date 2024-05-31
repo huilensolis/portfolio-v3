e6f0c513409d6408b56efd904025b5f3407e0898
Huilensolis <xywvanz@gmail.com>
2024-05-31 17:46:50 -0300

    add open graph to home page

---
+++ src/app/[lang]/opengraph-image.tsx
@@ -0,0 +1,71 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "Huilen Solis's Blog";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  // Font
+  const geistSemiBold = fetch(
+    new URL(
+      "/public/Geist-1.3.0/Geist-1.3.0/statics-ttf/Geist-SemiBold.ttf",
+      import.meta.url,
+    ),
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: "rgb(10 10 10 / 1)",
+          color: "rgb(250 250 250 / 1)",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          padding: "2rem",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+            fontSize: "5rem",
+            textWrap: "balance",
+            width: "80%",
+          }}
+        >
+          Huilen Solis
+        </h1>
+        <span style={{ fontSize: "2.5rem" }}>Full stack developer</span>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: await geistSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    },
+  );
+}
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -9,6 +9,49 @@ import Link from "next/link";
 import { TimeRange } from "@/components/feature/time-range/time-range.component";
 import Image from "next/image";
 import { CopyToClipboardBtn } from "@/components/feature/copy-to-clipboard";
+import { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { lang: "es" | "en" };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params: { lang } }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  const dictionary = await getDictionary(lang);
+
+  const ogImage = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/${lang}/opengraph-image`;
+
+  return {
+    title: "Huilen Solis Portfolio",
+    description: "Full Stack Developer",
+    creator: "Huilen Solis",
+    openGraph: {
+      images: [{ url: ogImage }, ...previousImages],
+      title: "Huilen Solis Portfolio",
+      description: "Full stack Developer",
+      type: "website",
+      locale: lang,
+    },
+    twitter: {
+      title: "Huilen Solis Portfolio",
+      description: "Full Stack Developer",
+      creator: dictionary.contact.twitter.at || undefined,
+      images: [
+        { url: ogImage, alt: "Huilen Solis Portfolio" },
+        ...previousImages,
+      ],
+      card: "summary_large_image",
+    },
+  };
+}
 
 export const dynamic = "force-static";
 
