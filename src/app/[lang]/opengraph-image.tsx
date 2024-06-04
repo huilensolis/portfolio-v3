02412574c674bf1b367efc03e30ae5dbe55a9da1
Huilensolis <xywvanz@gmail.com>
2024-06-03 22:01:30 -0300

    feat: change og image of home page

--- src/app/[lang]/opengraph-image.tsx
+++ src/app/[lang]/opengraph-image.tsx
@@ -6,8 +6,8 @@ export const runtime = "edge";
 // Image metadata
 export const alt = "Huilen Solis's Blog";
 export const size = {
-  width: 1200,
-  height: 630,
+  width: 800,
+  height: 400,
 };
 
 export const contentType = "image/png";
@@ -26,31 +26,51 @@ export default async function Image({
     ),
   ).then((res) => res.arrayBuffer());
 
+  const geistMedium = fetch(
+    new URL(
+      "/public/Geist-1.3.0/Geist-1.3.0/statics-ttf/Geist-Medium.ttf",
+      import.meta.url,
+    ),
+  ).then((res) => res.arrayBuffer());
+
   return new ImageResponse(
     (
       <div
         style={{
-          background: "rgb(10 10 10 / 1)",
+          // background: "rgb(10 10 10 / 1)",
           color: "rgb(250 250 250 / 1)",
           width: "100%",
           height: "100%",
           display: "flex",
+          alignItems: "center",
           justifyContent: "center",
           flexDirection: "column",
           padding: "2rem",
+          lineHeight: "0.75",
+          letterSpacing: "-0.025em",
+          backgroundImage:
+            "linear-gradient(207deg, rgba(126, 130, 255, 1) 2%, rgba(0, 0, 0, 1) 35%)",
         }}
       >
         <h1
           style={{
             textAlign: "center",
-            fontSize: "5rem",
+            fontSize: "4rem",
             textWrap: "balance",
-            width: "80%",
+            fontWeight: 400,
           }}
         >
           Huilen Solis
         </h1>
-        <span style={{ fontSize: "2.5rem" }}>Full stack developer</span>
+        <span
+          style={{
+            fontSize: "2rem",
+            color: "rgb(209 213 219 / 1)",
+            fontWeight: 300,
+          }}
+        >
+          Full stack developer
+        </span>
       </div>
     ),
     // ImageResponse options
@@ -64,6 +84,12 @@ export default async function Image({
           data: await geistSemiBold,
           style: "normal",
           weight: 400,
+        },
+        {
+          name: "Inter",
+          data: await geistMedium,
+          style: "normal",
+          weight: 300,
         },
       ],
     },
