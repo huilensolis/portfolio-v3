c5502917ff62f0728c6d67f4fa3e35a09a91e8a7
Huilensolis <xywvanz@gmail.com>
2024-06-08 14:45:59 -0300

    create theme switcher

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,5 +1,6 @@
 import { LangSwitcher } from "@/components/feature/lang-switcher";
 import { NavBar } from "@/components/feature/nav-bar";
+import { ThemeSwitcher } from "@/components/feature/theme-switcher";
 import { ReactNode } from "react";
 
 export default function Layout({
@@ -13,7 +14,10 @@ export default function Layout({
     <div className="flex flex-col py-16">
       <header className="flex justify-between w-full pb-10">
         <NavBar currentLang={lang} />
-        <LangSwitcher currentLang={lang} />
+        <div className="flex">
+          <ThemeSwitcher />
+          <LangSwitcher currentLang={lang} />
+        </div>
       </header>
       {children}
     </div>
---
+++ src/components/feature/theme-switcher/index.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button/button.component";
+import { MoonStar, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export function ThemeSwitcher() {
+  const [theme, setTheme] = useState<"dark" | "light">(() => {
+    if (typeof localStorage) return "light";
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      return "dark";
+    } else {
+      return "light";
+    }
+  });
+
+  useEffect(() => {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  function toggleTheme() {
+    if (localStorage.theme === "dark") {
+      // Whenever the user explicitly chooses light mode
+      localStorage.theme = "light";
+      setTheme("light");
+      return;
+    }
+
+    // Whenever the user explicitly chooses dark mode
+    localStorage.theme = "dark";
+    setTheme("dark");
+  }
+
+  return (
+    <Button variant="ghost" onClick={toggleTheme}>
+      {theme === "light" ? (
+        <MoonStar className="w-5 h-5" />
+      ) : (
+        <Sun className="w-5 h-5" />
+      )}
+    </Button>
+  );
+}
