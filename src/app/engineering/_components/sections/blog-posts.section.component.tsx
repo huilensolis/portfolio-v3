c9433026680f43218885cb9c227093f0b3a0b427
Huilensolis <xywvanz@gmail.com>
2024-07-13 19:01:27 -0300

    feat: styles

--- src/app/engineering/_components/sections/blog-posts.section.component.tsx
+++ src/app/engineering/_components/sections/blog-posts.section.component.tsx
@@ -12,7 +12,7 @@ export async function BlogPostsSection() {
         {[...posts, ...posts, ...posts, ...posts].map((blogPost, i) => (
           <li key={i} className="group">
             <Link href={`/engineering/blog/post/${blogPost.slug}`}>
-              <article className="flex flex-col gap-24 bg-gray-100 dark:bg-neutral-800/60 rounded-md border border-gray-200/40 dark:border-neutral-700/40 p-4 hover:dark:brightness-125 hover:bg-gray-200/60 duration-150 transition-all">
+              <article className="flex flex-col gap-24 bg-gray-100 dark:bg-neutral-800/60 rounded-md border border-gray-200/40 dark:border-neutral-700/40 p-4 hover:dark:brightness-125 hover:bg-gray-200/60 hover:dark:bg-neutral-800/60 duration-150 transition-all">
                 <header className="w-full flex justify-between">
                   <span className="font-medium">blog post</span>
                   <ArrowUpRight className="w-4 h-4 group-hover:scale-125 transition-all duration-150" />
--- src/app/engineering/_components/sections/projects.section.component.tsx
+++ src/app/engineering/_components/sections/projects.section.component.tsx
@@ -9,13 +9,13 @@ export async function ProjectsSection() {
         {projectList.map((project, i) => (
           <li
             key={i}
-            className="w-full overflow-y-hidden sticky bg-cm-indigo-bg"
+            className="w-full sticky dark:bg-cm-indigo-bg"
             style={{
               top: i * 100 + 100,
               transform: `rotate(${i % 2 === 0 ? Math.round(Math.random()) * 2 : -Math.round(Math.random()) * 2}deg)`,
             }}
           >
-            <article className="h-full bg-neutral-800/40 p-4 rounded-lg border-2 border-neutral-200/20 dark:border-neutral-800 flex flex-col gap-10">
+            <article className="h-full bg-gray-100 dark:bg-neutral-800/40 p-4 rounded-lg border-2 border-gray-200 dark:border-neutral-700/60 flex flex-col gap-10">
               <div className="flex flex-col gap-4">
                 <h1 className="text-2xl font-medium">{project.title}</h1>
                 <p className="font-medium">{project.description}</p>
