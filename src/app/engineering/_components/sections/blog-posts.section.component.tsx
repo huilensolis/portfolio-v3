9ecd90416b1f0eea49110d51291c9cd6f3df5405
Huilensolis <xywvanz@gmail.com>
2024-07-13 18:10:53 -0300

    add light mode for blog posts items

--- src/app/engineering/_components/sections/blog-posts.section.component.tsx
+++ src/app/engineering/_components/sections/blog-posts.section.component.tsx
@@ -10,12 +10,9 @@ export async function BlogPostsSection() {
       <h3 className="font-medium text-4xl">Blog Posts</h3>
       <ul className="w-full grid grid-cols-3 gap-4">
         {[...posts, ...posts, ...posts, ...posts].map((blogPost, i) => (
-          <li
-            key={i}
-            className="group hover:brightness-125 duration-150 transition-all"
-          >
+          <li key={i} className="group">
             <Link href={`/engineering/blog/post/${blogPost.slug}`}>
-              <article className="flex flex-col gap-24 bg-neutral-800/60 rounded-md border border-neutral-700/40 p-4">
+              <article className="flex flex-col gap-24 bg-gray-100 dark:bg-neutral-800/60 rounded-md border border-gray-200/40 dark:border-neutral-700/40 p-4 hover:dark:brightness-125 hover:bg-gray-200/60 duration-150 transition-all">
                 <header className="w-full flex justify-between">
                   <span className="font-medium">blog post</span>
                   <ArrowUpRight className="w-4 h-4 group-hover:scale-125 transition-all duration-150" />
