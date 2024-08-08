355a0e70ad2e6bad840a0f840b4fbef2049f0d91
Huilensolis <xywvanz@gmail.com>
2024-08-08 18:02:39 -0300

    refactor: project card

--- src/app/engineering/_components/sections/projects.section.component.tsx
+++ src/app/engineering/_components/sections/projects.section.component.tsx
@@ -1,32 +1,38 @@
 import projectList from "@/data/dictionary/projects.json";
+import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export async function ProjectsSection() {
   return (
     <section className="flex flex-col gap-10">
       <h3 className="text-4xl font-medium">Projects</h3>
-      <ul className="flex flex-col gap-10 relative min-h-screen">
+      <ul className="md:grid md:grid-cols-5 flex flex-col gap-5 w-full">
         {projectList.map((project, i) => (
           <li
             key={i}
-            className="w-full sticky dark:bg-cm-indigo-bg"
+            className="bg-gray-100 dark:bg-cm-indigo-bg border border-gray-200 rounded-md"
             style={{
-              top: i * 100 + 100,
-              transform: `rotate(${i % 2 === 0 ? Math.round(Math.random()) * 2 : -Math.round(Math.random()) * 2}deg)`,
+              gridColumn: `span ${project["grid-cols"]}`,
+              gridRow: `span ${project["grid-rows"]}`,
             }}
           >
-            <article className="h-full bg-gray-100 dark:bg-neutral-800/40 p-4 rounded-lg border-2 border-gray-200 dark:border-neutral-700/60 flex flex-col gap-10">
-              <div className="flex flex-col gap-4">
-                <h1 className="text-2xl font-medium">{project.title}</h1>
-                <p className="font-medium">{project.description}</p>
+            <article className="h-full w-full dark:bg-neutral-800/40 flex flex-col group">
+              <header className="flex justify-between items-center gap-4 p-4 text-neutral-600">
+                <h1 className="text-lg font-normal">{project.title}</h1>
+                <Link href={""}>
+                  <ArrowUpRight className="w-4 h-4 group-hover:scale-125 transition-all duration-150" />
+                </Link>
+              </header>
+              <div className="w-full h-full overflow-hidden">
+                <Image
+                  width={1000}
+                  height={700}
+                  src={project.cover_img}
+                  alt={project.description}
+                  className="w-full h-full object-cover object-left-top rounded-tl-[0.270rem] shadow-xl shadow-neutral-400 group-hover:shadow-neutral-500 translate-x-6 translate-y-10 group-hover:translate-y-6 transition-all duration-300"
+                />
               </div>
-              <Image
-                width={1000}
-                height={700}
-                src={project.cover_img}
-                alt={project.description}
-                className="w-full aspect-video object-cover object-left rounded-md"
-              />
             </article>
           </li>
         ))}
