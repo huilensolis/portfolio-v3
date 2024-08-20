a5751d57e4ca7fec0bff3e60736838406a37fb12
Huilensolis <xywvanz@gmail.com>
2024-08-19 22:43:14 -0300

    refactor: main text

--- src/app/_components/sections/main.section.component.tsx
+++ src/app/_components/sections/main.section.component.tsx
@@ -1,18 +1,31 @@
+import Image from "next/image";
+import cuddlyFerris from "../../../../public/cuddlyferris.svg";
+
 export function MainSection() {
   return (
     <main className="col-span-8">
-      <section className="flex flex-col gap-5 font-fraunces">
-        <h1 className="font-medium text-5xl tracking-tight text-balance text-stone-600">
-          I&apos;m <span className="text-amber-500 font-semibold">Huilen</span>,{" "}
+      <section className="flex flex-col gap-4 font-fraunces text-stone-500">
+        <h1 className="font-medium text-5xl tracking-tight text-balance">
+          I&apos;m{" "}
+          <span className="text-stone-700 font-semibold">Solis Huilen</span>.
         </h1>
-        <h2 className="text-3xl w-full tracking-tight text-balance text-stone-600">
-          A Software Engineer, developing Full Stack aplications with
-          Typescript, Next js, Node js, DrizzleORM, PostgreSQL and Rust.
-          <br />
-          <br />
-          On my free time, I learn Latin, read history books, go mountain
-          hiking, and draw.
+        <h2 className="text-3xl w-full tracking-tight text-pretty">
+          A <span className="text-stone-700">Software Engineer</span>,
+          developing Full Stack aplications with Typescript, Next js, Tailwind
+          CSS, Node js, DrizzleORM, PostgreSQL and Rust
+          <Image
+            src={cuddlyFerris}
+            width={40}
+            height={36}
+            alt="curstacean emoji"
+            className="inline mx-1"
+          />
+          .
         </h2>
+        <p className="tracking-tight text-3xl">
+          On my free time, I learn Latin, read history books with the company of
+          my cat Takumi, go mountain hiking, enjoy cooking, and draw.
+        </p>
       </section>
     </main>
   );
