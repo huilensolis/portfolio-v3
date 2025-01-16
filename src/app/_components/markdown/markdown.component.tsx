3cf78e65c7e9113cc30d61088cd186a11542f356
Huilensolis <xywvanz@gmail.com>
2025-01-16 16:47:10 -0300

    fix mdx remote issues with new version

--- src/app/_components/markdown/markdown.component.tsx
+++ src/app/_components/markdown/markdown.component.tsx
@@ -1,17 +1,8 @@
-import {serialize} from "next-mdx-remote/serialize"
 import { MdxRemoteComponent } from "./mdx-remote";
 
 export async function MarkDownMdx({ source }: { source: string }) {
-    // Serialize the markdown
-  const serializedSource = await serialize(source, {
-    mdxOptions: {
-      remarkPlugins: [],
-      rehypePlugins: [],
-    }
-  })
-
   return (
-    <div className="font-fraunces w-full h-full max-w-none pb-5 prose prose-neutral prose-headings:font-semibold prose-hr:my-1 prose-hr:mb-5 prose-h1:mb-5 prose-h2:mt-4 prose-h2:my-3">
+    <div className="font-fraunces w-full h-full max-w-none pb-5 prose prose-neutral prose-headings:font-semibold prose-hr:my-1 prose-hr:mb-5 prose-h1:mb-5 prose-h2:mt-4 prose-h2:my-3 prose-img:w-full">
       <MdxRemoteComponent serializedSource={source} />
     </div>
   );
