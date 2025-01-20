01f12ca7e91472a4c531bbb46bc5c75e76cd40bb
Huilensolis <xywvanz@gmail.com>
2025-01-20 13:07:15 -0300

    some tweaks

--- src/app/_components/sections/projects.section.component.tsx
+++ src/app/_components/sections/projects.section.component.tsx
@@ -3,48 +3,48 @@ import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
 export async function ProjectsSection() {
-	return projectList.map((project, i) => (
-		<>
-			<li
-				key={i}
-				className="lg:block hidden bg-gray-200/30 border border-gray-200 rounded-md"
-				style={{
-					gridColumn: `span ${project["grid-cols"]}`,
-					gridRow: `span ${project["grid-rows"]}`,
-				}}
-			>
-				<Link href={`/project/${project.title.trim().replaceAll(" ", "-")}`}>
-					<article className="h-full w-full flex flex-col group">
-						<header className="flex justify-between items-center gap-4 p-4 text-neutral-600">
-							<h2 className="text-lg font-normal">Project / {project.label}</h2>
-							<ArrowUpRight className="w-4 h-4 group-hover:scale-125 transition-all duration-150" />
-						</header>
-						<div className="w-full h-72 overflow-hidden flex items-center justify-center">
-							<h1 className="text-4xl font-fraunces font-medium">
-								{project.title}
-							</h1>
-						</div>
-					</article>
-				</Link>
-			</li>
-			<li
-				key={project.title}
-				className="lg:hidden block bg-gray-200/30 border border-gray-200 rounded-md"
-			>
-				<Link href={`/project/${project.title.trim().replaceAll(" ", "-")}`}>
-					<article className="h-full w-full flex flex-col group">
-						<header className="flex justify-between items-center gap-4 p-4 text-neutral-600">
-							<h2 className="text-lg font-normal">Project / {project.label}</h2>
-							<ArrowUpRight className="w-4 h-4 group-hover:scale-125 transition-all duration-150" />
-						</header>
-						<div className="w-full h-72 overflow-hidden flex items-center justify-center">
-							<h1 className="text-4xl font-fraunces font-medium">
-								{project.title}
-							</h1>
-						</div>
-					</article>
-				</Link>
-			</li>
-		</>
-	));
+    return projectList.map((project, i) => (
+        <>
+            <li
+                key={i}
+                className="lg:block hidden bg-gray-200/30 border border-gray-200 rounded-md"
+                style={{
+                    gridColumn: `span ${project["grid-cols"]}`,
+                    gridRow: `span ${project["grid-rows"]}`,
+                }}
+            >
+                <Link href={`/project/${project.title.trim().replaceAll(" ", "-")}`}>
+                    <article className="h-full w-full flex flex-col group">
+                        <header className="flex justify-between items-center gap-4 p-4 text-neutral-600">
+                            <span className="font-medium">project / {project.label}</span>
+                            <ArrowUpRight className="w-4 h-4 group-hover:scale-125 transition-all duration-150" />
+                        </header>
+                        <div className="w-full h-72 overflow-hidden flex items-center justify-center">
+                            <h1 className="text-4xl font-fraunces font-medium">
+                                {project.title}
+                            </h1>
+                        </div>
+                    </article>
+                </Link>
+            </li>
+            <li
+                key={project.title}
+                className="lg:hidden block bg-gray-200/30 border border-gray-200 rounded-md"
+            >
+                <Link href={`/project/${project.title.trim().replaceAll(" ", "-")}`}>
+                    <article className="h-full w-full flex flex-col group">
+                        <header className="flex justify-between items-center gap-4 p-4 text-neutral-600">
+                            <h2 className="text-lg font-normal">Project / {project.label}</h2>
+                            <ArrowUpRight className="w-4 h-4 group-hover:scale-125 transition-all duration-150" />
+                        </header>
+                        <div className="w-full h-72 overflow-hidden flex items-center justify-center">
+                            <h1 className="text-4xl font-fraunces font-medium">
+                                {project.title}
+                            </h1>
+                        </div>
+                    </article>
+                </Link>
+            </li>
+        </>
+    ));
 }
