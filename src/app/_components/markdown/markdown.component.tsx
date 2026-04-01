c12eeb51ad44c798859c78d7cab3467bbf6546f1
Huilensolis <xywvanz@gmail.com>
2026-04-01 20:54:44 -0300

    make first letter of sentence bigger in markdown

--- src/app/_components/markdown/markdown.component.tsx
+++ src/app/_components/markdown/markdown.component.tsx
@@ -2,7 +2,7 @@ import { MdxRemoteComponent } from "./mdx-remote";
 
 export async function MarkDownMdx({ source }: { source: string }) {
 	return (
-		<div className="font-fraunces text-[28px] leading-relaxed w-full h-full max-w-none pb-5 prose prose-neutral prose-headings:font-normal prose-hr:my-1 prose-hr:mb-5 prose-h1:mb-5 prose-h2:mt-4 prose-h2:my-3 prose-img:w-full">
+		<div className="prose-p:first-letter:text-9xl prose-p:first-letter:uppercase prose-p:first-letter:float-left prose-p:first-letter:mr-7 prose-p:mb-16 font-fraunces text-[28px] leading-relaxed w-full h-full max-w-none pb-5 prose prose-neutral prose-headings:font-normal prose-hr:my-1 prose-hr:mb-5 prose-h1:mb-5 prose-h2:mt-4 prose-h2:my-3 prose-img:w-full">
 			<MdxRemoteComponent serializedSource={source} />
 		</div>
 	);
